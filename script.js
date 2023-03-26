70bdb53b45b2feb3cfd5c4d7bc76923af6f98eca
Brynna <68093791+bwatring@users.noreply.github.com>
2023-03-26 11:07:50 -0500

    Three files used to create website

---
+++ script.js
@@ -0,0 +1,64 @@
+let weather = {
+  apiKey: "c88ce85eaf9adfa02bf2f900d3976914",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => {
+        if (!response.ok) {
+          alert("No weather found.");
+          throw new Error("No weather found.");
+        }
+        return response.json();
+      })
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText =
+      "Wind speed: " + speed + " km/h";
+    document.querySelector(".weather").classList.remove("loading");
+    document.body.style.backgroundImage =
+      "url('https://www.nasa.gov/sites/default/files/thumbnails/image/web_first_images_release.png')";
+  },
+  whatToWear: function(data){
+	const { temp } = data.main;
+	document.querySelector(".temp").innerText = temp + "°C";
+	if(temp> 24){
+	return("I think you should wear shorts its hot out!")
+		}
+		else{
+		return("You should wear a sweater! Its a bit chilly out!")
+		}
+  },
+	
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
+
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
+weather.fetchWeather("San Antonio");
